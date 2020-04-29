6fa138b4252f2761a21cebc0c05e9de8e121e1fa
sid <s3kala@uwaterloo.ca>
2020-04-29 16:16:00 -0400

    fixed compile error in sample client

--- samples/clients/test_client.cc
+++ samples/clients/test_client.cc
@@ -54,7 +54,7 @@ int main(int argc, char* argv[]) {
             std::cout << "The shortest path is: ";
             for(auto it : sp)
                std::cout << it << ' ';
-            std::cout << endl;
+            std::cout << std::endl;
         }
     }
 }
